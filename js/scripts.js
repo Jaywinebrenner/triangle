f857fbb625033fc560f4ad2a8dd1623ee45e74a9
Ike Esquivel-Pilloud and Jay Winebrenner <jay@>
2019-10-24 16:22:30 -0700

    add successful else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-$(document).ready(function(){
-  var sideAValue = parseInt($("#trianglea").val());
-  var sideBValue = parseInt($("#triangleb").val());
-  var sideCValue = parseInt($("#trianglec").val());
+$(document).ready(function() {
+  $("#triangleform").submit(function(event) {
+      var sideAValue = parseInt($("#trianglea").val());
+      var sideBValue = parseInt($("#triangleb").val());
+      var sideCValue = parseInt($("#trianglec").val());
 
-  if (sideAValue === sideBValue && sideBValue === sideCValue)
+      if (sideAValue === sideBValue) {
+        //  block of code to be executed if condition1 is true
+      } else if (sideAValue === sideCValue) {
+        //  block of code to be executed if the condition1 is false and condition2 is true
+      } else {
+       (sideBValue === sideCValue)
+      };
+      alert("equalateratlll!")
 
+
+
+      event.preventDefault();
+  });
 });

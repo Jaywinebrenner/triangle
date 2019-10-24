09f1775b3bc0782aa0d84a451d8f42655fc8d4b6
Student <example@example.com>
2019-10-24 15:28:30 -0700

    add initial files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  var sideAValue = parseInt($("#trianglea").val());
+  var sideBValue = parseInt($("#triangleb").val());
+  var sideCValue = parseInt($("#trianglec").val());
+
+  if (sideAValue === sideBValue && sideBValue === sideCValue)
+
+});
